c098dcd374d0f8f5a63923974d67524ced17bc94
Lennart Prelle <me@lennartprelle.de>
2013-01-11 13:34:20 +0100

    move displayed sum of events to a helper

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -9,8 +9,6 @@ class CalendarController < ApplicationController
     @shown_month = Date.civil(@year, @month)
 
     @event_strips = Event.event_strips_for_month(@shown_month)
-
-    @sum_user_events = current_user.events.length
   end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def display_sum_user_events
+    "<li><b>Benutzte Urlaubstag</b>: #{current_user.events.length} </li>".html_safe
+  end
 end
