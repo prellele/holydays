2071e4506e6e6825f6537e435e1e1952b9697a83
Lennart Prelle <me@lennartprelle.de>
2013-01-11 17:27:48 +0100

    ui tweaks; new nice stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
   def display_sum_user_events
-    "<li><b>Benutzte Urlaubstag</b>: #{current_user.events.length} </li>".html_safe
+    "<li><b>Benutzte Urlaubstage</b>: #{current_user.events.length} </li>".html_safe
   end
 end
