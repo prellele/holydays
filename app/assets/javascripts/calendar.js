4786a5fd2cafad1e16ef39ab2272f8ca2419c4e7
Lennart Prelle <me@prellele.de>
2013-08-29 16:11:53 +0200

    set clicked day-header as start and enddate, some new css

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
   $(".ec-day-header").on("click", function () {
+     var date = $(this).data("date");
+     var spDate = date.split('-');
+
+     $("#event_start_at_1i option").filter(function() {
+       return $(this).text() == spDate[0]; 
+     }).prop('selected', true);
+     $("#event_start_at_2i option").filter(function() {
+       return $(this).text() == spDate[1]; 
+     }).prop('selected', true);
+     $("#event_start_at_3i option").filter(function() {
+       return $(this).text() == spDate[2]; 
+     }).prop('selected', true);
+
+     $("#event_end_at_1i option").filter(function() {
+       return $(this).text() == spDate[0];
+     }).prop('selected', true);
+     $("#event_end_at_2i option").filter(function() {
+       return $(this).text() == spDate[1];
+     }).prop('selected', true);
+     $("#event_end_at_3i option").filter(function() {
+       return $(this).text() == spDate[2]; 
+     }).prop('selected', true);
+
      $('#addEventDialog').modal('toggle');
   });
 });
